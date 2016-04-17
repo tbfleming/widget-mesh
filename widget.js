dbd4fbfc0c634fe5f5db1fe0ad61b6860296e49d
Todd Fleming <tbfleming@gmail.com>
2016-04-17 19:14:02 -0400

    Use Clipper

--- widget.js
+++ widget.js
@@ -29,6 +29,7 @@ requirejs.config({
         // SmoothieCharts: '//smoothiecharts.org/smoothie',
         Three: '//i2dcui.appspot.com/slingshot?url=http://threejs.org/build/three.min.js',
         ThreeSTLLoader: '//i2dcui.appspot.com/slingshot?url=http://threejs.org/examples/js/loaders/STLLoader.js',
+        Clipper: '//i2dcui.appspot.com/js/clipper/clipper_unminified',
     },
     shim: {
         // See require.js docs for how to define dependencies that
@@ -77,7 +78,7 @@ cprequire_test(["inline:org-jscut-widget-prismatic"], function(myWidget) {
 } /*end_test*/ );
 
 // This is the main definition of your widget. Give it a unique name.
-cpdefine("inline:org-jscut-widget-prismatic", ["chilipeppr_ready", "Three", "ThreeSTLLoader"], function () {
+cpdefine("inline:org-jscut-widget-prismatic", ["chilipeppr_ready", "Three", "ThreeSTLLoader", "Clipper"], function () {
     return {
         /**
          * The ID of the widget. You must define this and make it unique.
@@ -169,9 +170,58 @@ cpdefine("inline:org-jscut-widget-prismatic", ["chilipeppr_ready", "Three", "Thr
                         planeVertices[i * 9 + j] = planeTriangles[i][j];
                 planeGeometry.addAttribute('position', new THREE.BufferAttribute(planeVertices, 3));
 
-                let planeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+                let planeMaterial = new THREE.MeshBasicMaterial({ color: 0xC0C0C0 });
                 let planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
                 chilipeppr.publish('/com-chilipeppr-widget-3dviewer/sceneadd', planeMesh);
+
+                let scale = 1000000;
+                let planes = [];
+                for (let i = 0; i < planeTriangles.length; ++i) {
+                    if (planes.length === 0 || planeTriangles[i][2] != planes[planes.length - 1].z)
+                        planes.push({ z: planeTriangles[i][2], paths: [] });
+                    planes[planes.length - 1].paths.push([
+                        { X: planeTriangles[i][0] * scale, Y: planeTriangles[i][1] * scale },
+                        { X: planeTriangles[i][3] * scale, Y: planeTriangles[i][4] * scale },
+                        { X: planeTriangles[i][6] * scale, Y: planeTriangles[i][7] * scale }]);
+                }
+
+                let co = new ClipperLib.ClipperOffset(2.0, scale / 1000);
+                for (let i = 0; i < planes.length; ++i) {
+                    let paths = planes[i].paths;
+                    planes[i].paths = [];
+                    let first = true;
+                    while (paths.length && paths[0].length) {
+                        co.AddPaths(paths, ClipperLib.JoinType.jtRound, ClipperLib.EndType.etClosedPolygon);
+                        if (first)
+                            co.Execute(paths, 1);
+                        else
+                            co.Execute(paths, -2 * scale);
+                        first = false;
+                        co.Clear();
+                        planes[i].paths = planes[i].paths.concat(paths);
+
+                    }
+                }
+
+                function cleanup(coord) {
+                    return (coord / scale).toFixed(4);
+                }
+
+                let gcode = '';
+                for (let i = 1; i < planes.length; ++i) {
+                    let paths = planes[i].paths;
+                    for (let j = 0; j < paths.length; ++j) {
+                        let path = paths[j];
+                        path.push(path[0]);
+                        gcode += 'G0 Z100\n';
+                        gcode += 'G0 X' + cleanup(path[0].X) + ' Y' + cleanup(path[0].Y);
+                        gcode += 'G0 Z' + planes[i].z.toFixed(4) + '\n';
+                        for (let k = 1; k < path.length; ++k) {
+                            gcode += 'G1 X' + cleanup(path[k].X) + ' Y' + cleanup(path[k].Y) + '\n';
+                        }
+                    }
+                }
+                chilipeppr.publish('/com-chilipeppr-elem-dragdrop/ondropped', gcode);
 
                 return false;
             } else
